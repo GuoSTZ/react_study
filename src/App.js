c7dfd9208f273c8b6a854cd86a157f3bd7708b3e
guosheng <guosheng@mchz.com.cn>
2021-04-19 14:55:02 +0800

    feat: 样式调整

--- src/App.js
+++ src/App.js
@@ -3,9 +3,92 @@ import ListMenu from './listDemo/listMenu/index';
 import './App.css';
 
 export default class App extends React.Component {
+
   render() {
+    const dataSource = [
+      {
+        name: '账号管理',
+        children: []
+      },
+      {
+        name: '代码管理',
+        children: [
+          { name: '存储过程/函数/包', status: 0 },
+          { name: '触发器', status: 1 },
+          { name: '视图', status: 0 }
+        ]
+      },
+      {
+        name: '对象管理',
+        children: [
+          { name: '敏感对象', status: 0 },
+          { name: '业务用户对象', status: 1 },
+          { name: '系统对象', status: 0 },
+          { name: '表空间', status: 1 },
+          { name: '数据库', status: 0 }
+        ]
+      },
+      {
+        name: '授权管理',
+        children: [
+          { name: '数据库角色权限', status: 0 },
+          { name: '数据库对象权限', status: 1 },
+          { name: '数据库系统权限', status: 0 }
+        ]
+      },
+      {
+        name: '权限模块',
+        children: [
+          { name: '权限一', status: 0 },
+          { name: '权限二', status: 1 },
+          { name: '权限三', status: 0 }
+        ]
+      },
+      {
+        name: '权限模块',
+        children: [
+          { name: '权限一', status: 0 },
+          { name: '权限二', status: 1 },
+          { name: '权限三', status: 0 }
+        ]
+      },
+      {
+        name: '权限模块',
+        children: [
+          { name: '权限一', status: 0 },
+          { name: '权限二', status: 1 },
+          { name: '权限三', status: 0 }
+        ]
+      },
+      {
+        name: '权限模块',
+        children: [
+          { name: '权限一', status: 0 },
+          { name: '权限二', status: 1 },
+          { name: '权限三', status: 0 }
+        ]
+      }
+    ]
+    const selectData = [
+      {value: 1, label: 'aaa'},
+      {value: 2, label: 'bbb'},
+      {value: 3, label: 'ccc'},
+      {value: 4, label: 'ddd'},
+      {value: 5, label: 'eee'}
+    ]
     return (
-      <ListMenu />
+      <ListMenu
+        dataSource={dataSource}
+        openText="启用"
+        closeText="停用"
+        onClick={(status, selectValue) => console.log(status, selectValue, '----')}
+        style={{width: 260, margin: 20}}
+        selectProps={{
+          placeholder: "请选择数据源",
+          data: selectData,
+          onChange: value => console.log(value)
+        }}
+      />
     );
   }
 }
--- src/listDemo/listMenu/index.js
+++ src/listDemo/listMenu/index.js
@@ -1,74 +1,100 @@
-import React from 'react';
-import { List, Button, message } from 'antd';
+import React, { Component } from 'react';
+import { List, Button, Select } from 'antd';
 import './index.css';
 
-export default class ListMenu extends React.PureComponent {
-  onClick() {
-
+export default class ListMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: undefined
+    }
   }
-  renderActions(status, style) {
+  renderActions(status) {
+    let open = '开启';
+    let close = '关闭';
+    const { openText, closeText, onClick } = this.props;
+    if (openText) {
+      open = openText;
+    }
+    if (closeText) {
+      close = closeText;
+    }
     return (
-      <Button style={style}>{status === 0 ? '开启' : '关闭'}</Button>
+      <Button onClick={onClick.bind(this, status, this.state.selectValue)}>{status === 1 ? open : close}</Button>
     )
   }
+  hideNoData(children) {
+    if (Array.isArray(children) && children.length !== 0) {
+      return '';
+    } else {
+      return 'hide';
+    }
+  }
+  // 数据源容错处理
+  ArrayFaultTolerant(dataSource) {
+    if (Array.isArray(dataSource)) {
+      return dataSource;
+    } else {
+      return [];
+    }
+  }
+  onChange = value => {
+    const onChange = this.props.selectProps.onChange;
+    onChange(value);
+    this.setState({
+      selectValue: value
+    });
+  }
   render() {
-    const data = [
-      {
-        name: '账号管理',
-        children: []
-      },
-      {
-        name: '代码管理',
-        children: [
-          { name: '存储过程/函数/包', status: 0 },
-          { name: '触发器', status: 1 },
-          { name: '视图', status: 0 }
-        ]
-      },
-      {
-        name: '对象管理',
-        children: [
-          { name: '敏感对象', status: 0 },
-          { name: '业务用户对象', status: 1 },
-          { name: '系统对象', status: 0 },
-          { name: '表空间', status: 1 },
-          { name: '数据库', status: 0 }
-        ]
-      },
-      {
-        name: '授权管理',
-        children: [
-          { name: '数据库角色权限', status: 0 },
-          { name: '数据库对象权限', status: 1 },
-          { name: '数据库系统权限', status: 0 }
-        ]
-      }
-    ]
+    const { 
+      dataSource, 
+      openText, 
+      closeText, 
+      onClick, 
+      selectProps,
+      style,
+      ...otherProps 
+    } = this.props;
+    const {data, onChange, ...otherSelectProps} = selectProps;
     return (
-      <List
-        dataSource={data}
-        style={{ width: 430, margin: 100, minWidth: 430 }}
-        split={false}
-        renderItem={item => (
-          <List.Item
-            className='cccc'
-          >
-            <List
-              className={item.children.length === 0 ? 'aaaa' : ''}
-              header={<div>{item.name}</div>}
-              dataSource={item.children}
-              split={false}
-              renderItem={child => (
-                <List.Item
-                  actions={[this.renderActions(child.status)]}
-                >
-                  {child.name}
-                </List.Item>
-              )}
-            />
-          </List.Item>
-        )}
-      />
+      <div className='wrap' style={style}>
+        <Select 
+          {...otherSelectProps}
+          showSearch
+          optionFilterProp="children"
+          onChange={this.onChange}
+        >
+          {
+            data.map((item, index) => (
+              <Select.Option value={item.value} key={index}>{item.label}</Select.Option>
+            ))
+          }
+        </Select>
+        <List
+          dataSource={this.ArrayFaultTolerant(dataSource)}
+          split={false}
+          {...otherProps}
+          renderItem={item => (
+            <List.Item
+              className='two'
+            >
+              <List
+                className={this.hideNoData(item.children)}
+                header={<div>{item.name}</div>}
+                dataSource={this.ArrayFaultTolerant(item.children)}
+                split={false}
+                renderItem={child => (
+                  <List.Item
+                    actions={[this.renderActions(child.status)]}
+                  >
+                    {child.name}
+                  </List.Item>
+                )}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
     )
   }
 }
